63b2d5a9c85e5b94b719eb635a386d0dbc404268
lou <dustyfingers01257@gmail.com>
2021-04-17 17:48:48 -0600

    built out the email and phone section using the container widget

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,49 @@ class MyApp extends StatelessWidget {
                 "Software Developer",
                 style: TextStyle(
                     fontSize: 20.0,
-                    color: Colors.blue[50],
+                    color: Colors.white,
                     fontFamily: 'Source Sans Pro',
                     letterSpacing: 2.0),
-              )
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                color: Colors.blue.shade300,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text('1-800-123-1234',
+                        style: TextStyle(
+                            fontFamily: 'Source Sans Pro',
+                            color: Colors.white,
+                            fontSize: 20.0))
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.blue.shade300,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text('email@address.com',
+                        style: TextStyle(
+                            fontFamily: 'Source Sans Pro',
+                            color: Colors.white,
+                            fontSize: 20.0))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
