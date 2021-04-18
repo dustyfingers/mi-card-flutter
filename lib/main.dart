a69e5301ea7ecba5020cecc2281cfeedca76bf4d
lou <dustyfingers01257@gmail.com>
2021-04-17 18:03:22 -0600

    reformatted to use the Card and ListTile widgets and completed the 'business card' project

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue[700],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -29,47 +30,51 @@ class MyApp extends StatelessWidget {
                 "Software Developer",
                 style: TextStyle(
                     fontSize: 20.0,
-                    color: Colors.white,
+                    color: Colors.blue.shade100,
                     fontFamily: 'Source Sans Pro',
                     letterSpacing: 2.0),
               ),
-              SizedBox(height: 10.0),
-              Container(
-                color: Colors.blue.shade300,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 10.0),
-                    Text('1-800-123-1234',
-                        style: TextStyle(
-                            fontFamily: 'Source Sans Pro',
-                            color: Colors.white,
-                            fontSize: 20.0))
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 170.0,
+                child: Divider(
+                  color: Colors.blue.shade300,
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.blue.shade300,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    '1-800-123-1234',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
                       color: Colors.white,
+                      fontSize: 20.0,
                     ),
-                    SizedBox(width: 10.0),
-                    Text('email@address.com',
-                        style: TextStyle(
-                            fontFamily: 'Source Sans Pro',
-                            color: Colors.white,
-                            fontSize: 20.0))
-                  ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.blue.shade300,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'email@address.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               ),
             ],
